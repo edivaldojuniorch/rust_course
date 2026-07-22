5a9897f54202e4d83d68d22aac914c708ccb256b
Edivaldo Junior <joseedivaldocg@hotmail.com>
2026-07-22 23:33:09 +0100

    feat: creation mod and using references for the day files.

---
+++ src/day05.rs
@@ -0,0 +1,8 @@
+pub mod day05{
+    pub fn day_description(){
+        println!("--------------------------------");
+        println!("------ DAY 05 -----");
+        println!("----We are going to use complext data types today -----");
+
+    }
+}
