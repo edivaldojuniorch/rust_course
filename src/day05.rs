9daca2b848f44defd37e70ad45d8d3e2d5714788
Edivaldo Junior <joseedivaldocg@hotmail.com>
2026-07-23 00:13:10 +0100

    feat: vectors creation and updating option 1.

--- src/day05.rs
+++ src/day05.rs
@@ -4,5 +4,19 @@ pub mod day05{
         println!("------ DAY 05 -----");
         println!("----We are going to use complext data types today -----");
 
+
+        // creation vector
+        vector_creation();
+
+    }
+
+    fn vector_creation(){
+
+        // mutable vector created empty then updated. 
+        let mut v1: Vec<i32> = Vec::new();
+
+        v1.push(1);
+        v1.push(5);
+        v1.push(67);
     }
 }
